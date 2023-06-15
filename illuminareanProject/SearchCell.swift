cefe9bf9682dd163fdc1a99ca89de093bf9af86a
WONGI HONG <dt95@naver.com>
2023-06-15 22:55:40 +0900

    ADD : tableView 셀 생성

---
+++ illuminareanProject/SearchCell.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class SearchCell: UITableViewCell {
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userUrlLabel: UILabel!
+}
