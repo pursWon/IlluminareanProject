505915d560a7c8b7909ca671bcee4fa2b8ce027c
WONGI HONG <dt95@naver.com>
2023-06-16 15:27:18 +0900

    MOD : 데이터 모델 이름 수정

--- illuminareanProject/Model/Users.swift
+++ illuminareanProject/Model/Users.swift
@@ -1,43 +1,11 @@
 import Foundation
-import Moya
 
-enum GitHubApi {
-    case searchUser(query: String)
+struct Users: Decodable {
+    let items: [Inform]
 }
 
-extension GitHubApi: TargetType {
-    var baseURL: URL {
-        let url: String = "https://api.github.com"
-        return URL(string: url)!
-    }
-    
-    var path: String {
-        switch self {
-        case .searchUser:
-            return "search/users"
-        }
-    }
-    
-    var method: Moya.Method {
-        return .get
-    }
-    
-    var sampleData: Data {
-        return Data()
-    }
-    
-    var task: Task {
-        switch self {
-        case .searchUser(let query):
-            return .requestParameters(parameters: ["q" : query], encoding: URLEncoding.default)
-        }
-    }
-    
-    var validationType: Moya.ValidationType {
-        return .successAndRedirectCodes
-    }
-    
-    var headers: [String : String]? {
-        return ["Accept": "application/vnd.github+json", "Authorization": "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"]
-    }
+struct Inform: Decodable {
+    let login: String
+    let avatar_url: String
+    let html_url: String
 }
