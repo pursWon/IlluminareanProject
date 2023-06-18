032bd63054a29bfec5893ead7c34fe9339f10cd4
WONGI HONG <dt95@naver.com>
2023-06-19 02:01:43 +0900

    MOD : UI를 코드로 그리기

--- illuminareanProject/Controller/SearchViewController.swift
+++ illuminareanProject/Controller/SearchViewController.swift
@@ -4,29 +4,64 @@ import Kingfisher
 import Then
 
 class SearchViewController: UIViewController {
-    @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var emptyLabel: UILabel!
-    @IBOutlet weak var cancelButton: UIButton!
-    
     let viewModel = SearchViewModel()
+    let searchBar: UISearchBar = UISearchBar().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    let searchButton: UIButton = UIButton().then {
+        let config = UIImage.SymbolConfiguration(pointSize: 25, weight: .regular, scale: .default)
+        let image = UIImage(systemName: "magnifyingglass", withConfiguration: config)
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setImage(image, for: .normal)
+        $0.tintColor = .orange
+    }
+    
+    let cancelButton: UIButton = UIButton().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setTitle("취소", for: .normal)
+        $0.setTitleColor(.red, for: .normal)
+    }
+    
+    let emptyLabel: UILabel = UILabel().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "검색 결과가 존재하지 않습니다."
+        $0.font = .boldSystemFont(ofSize: 21)
+    }
+    
+    var tableView: UITableView = UITableView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setHiddenButton()
+        setView()
+        setContraints()
         setTableView()
         setSearchBar()
+        setButtonAction()
+        setHiddenButton()
     }
     
-    func setHiddenButton() {
-        cancelButton.isHidden = true
-        emptyLabel.isHidden = true
+    func setView() {
+        let views: [UIView] = [tableView, searchBar, searchButton, cancelButton, emptyLabel]
+        view.backgroundColor = .white
+        
+        views.forEach {
+            view.addSubview($0)
+        }
     }
     
     func setTableView() {
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.register(CustomCell.self, forCellReuseIdentifier: CustomCell.identifier)
+    }
+    
+    func setButtonAction() {
+        searchButton.addTarget(self, action: #selector(searchButtonClicked), for: .touchUpInside)
+        cancelButton.addTarget(self, action: #selector(cancelButtonClicked), for: .touchUpInside)
     }
     
     func setSearchBar() {
@@ -37,13 +72,38 @@ class SearchViewController: UIViewController {
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).leftView = nil
     }
     
-    @IBAction func searchButton(_ sender: UIButton) {
+    func setHiddenButton() {
+        cancelButton.isHidden = true
+        emptyLabel.isHidden = true
+    }
+    
+    func setContraints() {
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 70).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
+        searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+        searchBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        searchBar.bottomAnchor.constraint(equalTo: tableView.topAnchor, constant: 0).isActive = true
+        searchBar.centerYAnchor.constraint(equalTo: searchButton.centerYAnchor).isActive = true
+        searchButton.leadingAnchor.constraint(equalTo: searchBar.trailingAnchor, constant: 10).isActive = true
+        searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        searchButton.topAnchor.constraint(equalTo: searchBar.topAnchor, constant: 0).isActive = true
+        searchButton.centerYAnchor.constraint(equalTo: searchBar.centerYAnchor).isActive = true
+        cancelButton.trailingAnchor.constraint(equalTo: searchButton.leadingAnchor, constant: -30).isActive = true
+        cancelButton.centerYAnchor.constraint(equalTo: searchBar.centerYAnchor).isActive = true
+        emptyLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        emptyLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    @objc func searchButtonClicked() {
         guard let text = searchBar.text else { return }
         
         viewModel.setProvideData(query: text, emptyLabel: emptyLabel, tableView: tableView)
     }
     
-    @IBAction func cancelButton(_ sender: UIButton) {
+    @objc func cancelButtonClicked() {
         searchBar.text = ""
         cancelButton.isHidden = true
     }
@@ -55,16 +115,16 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "searchCell", for: indexPath) as? SearchCell else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath) as? CustomCell else { return UITableViewCell() }
         
-        cell.userNameLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        cell.userNameLabel.text = viewModel.userInform[indexPath.row].login
-        cell.userUrlLabel.text = viewModel.userInform[indexPath.row].html_url
+        cell.nameLabel.font = .boldSystemFont(ofSize: 20)
+        cell.nameLabel.text = viewModel.userInform[indexPath.row].login
+        cell.urlLabel.text = viewModel.userInform[indexPath.row].html_url
         
         if viewModel.imageURL.count == viewModel.userInform.count {
             cell.userImageView.kf.setImage(with: viewModel.imageURL[indexPath.row])
         }
-     
+        
         return cell
     }
     
@@ -75,7 +135,7 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
+        return 130
     }
 }
 
@@ -90,3 +150,4 @@ extension SearchViewController: UISearchBarDelegate {
         }
     }
 }
+
