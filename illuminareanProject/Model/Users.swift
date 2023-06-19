49444b9ec7b4199ec2510027d03b2323ddb93a6d
WONGI HONG <dt95@naver.com>
2023-06-19 23:53:36 +0900

    MOD : Model 변수명 Camel 형식으로 변경

--- illuminareanProject/Model/Users.swift
+++ illuminareanProject/Model/Users.swift
@@ -1,11 +1,23 @@
 import Foundation
 
 struct Users: Decodable {
+    let totalCount: Int
     let items: [Inform]
+    
+    enum CodingKeys: String, CodingKey {
+        case totalCount = "total_count"
+        case items
+    }
 }
 
 struct Inform: Decodable {
     let login: String
-    let avatar_url: String
-    let html_url: String
+    let avatarUrl: String
+    let htmlUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case avatarUrl = "avatar_url"
+        case htmlUrl = "html_url"
+    }
 }
