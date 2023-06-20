04c906e4da2c39b5274ba19b71612b0ad5039638
WONGI HONG <dt95@naver.com>
2023-06-20 23:29:15 +0900

    MOD : UIProtocol 추가 및 함수 내 코드 변경

--- illuminareanProject/View/SearchCell.swift
+++ illuminareanProject/View/SearchCell.swift
@@ -1,8 +1,11 @@
 import UIKit
+import Then
 
-class CustomCell: UITableViewCell {
+class CustomCell: UITableViewCell, UIProtocol {
+    // MARK: Properties
     static let identifier = "CustomCell"
     
+    // MARK: UI
     let nameLabel: UILabel = UILabel().then {
         $0.text = "name"
         $0.font = .boldSystemFont(ofSize: 20)
@@ -16,41 +19,47 @@ class CustomCell: UITableViewCell {
     }
     
     let userImageView: UIImageView = UIImageView().then {
-        $0.translatesAutoresizingMaskIntoConstraints = false
         $0.backgroundColor = .red
+        $0.translatesAutoresizingMaskIntoConstraints = false
     }
     
+    // MARK: Init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        addContentView()
-        autoLayouyt()
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func addContentView() {
-        let cellContents = [nameLabel, urlLabel, userImageView]
-        
-        cellContents.forEach {
-            contentView.addSubview($0)
-        }
+    // MARK: Configure
+    func configure() {
+        setViews()
+        setContraints()
     }
     
-    func autoLayouyt() {
+    func setViews() {
+        let views: [UIView] = [nameLabel, urlLabel, userImageView]
+        views.forEach { contentView.addSubview($0) }
+    }
+    
+    func setContraints() {
         nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
         nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -50).isActive = true
         nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 150).isActive = true
         nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -100).isActive = true
+        
         urlLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5).isActive = true
         urlLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -30).isActive = true
         urlLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 150).isActive = true
         urlLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20).isActive = true
+        
         userImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
         userImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
         userImageView.trailingAnchor.constraint(equalTo: urlLabel.leadingAnchor, constant: -30).isActive = true
         userImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
     }
 }
+
