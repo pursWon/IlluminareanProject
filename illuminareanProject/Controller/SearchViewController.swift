fcbbec75a6e252d68394eabdf32e3f03062fc78f
WONGI HONG <dt95@naver.com>
2023-06-20 23:24:50 +0900

    MOD : SearchVC 내의 함수명 수정

--- illuminareanProject/Controller/SearchViewController.swift
+++ illuminareanProject/Controller/SearchViewController.swift
@@ -4,26 +4,32 @@ import Kingfisher
 import Then
 
 class SearchViewController: UIViewController {
+    // MARK: Properties
     let viewModel = SearchViewModel()
     let searchView = SearchView()
+    let perPage: Int = 30
+    
+    // MARK: View Life Cycle
+    override func loadView() {
+        self.view = searchView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view = searchView
-        
-        setTableView()
-        setSearchBar()
+        configure()
+    }
+    
+    // MARK: Configure
+    func configure() {
+        setupDelegates()
         setButtonAction()
     }
     
-    func setTableView() {
+    func setupDelegates() {
         searchView.tableView.delegate = self
         searchView.tableView.dataSource = self
         searchView.tableView.prefetchDataSource = self
-    }
-    
-    func setSearchBar() {
         searchView.searchBar.delegate = self
     }
     
@@ -32,26 +38,31 @@ class SearchViewController: UIViewController {
         searchView.cancelButton.addTarget(self, action: #selector(cancelButtonClicked), for: .touchUpInside)
     }
     
+    // MARK: ETC
     func fetchData() {
         guard let text = searchView.searchBar.text else { return }
-        
-        viewModel.setProvideData(query: (text, viewModel.pageNum), emptyLabel: searchView.emptyLabel, tableView: searchView.tableView)
+        viewModel.getUserData(param: (text, viewModel.pageNum), emptyLabel: searchView.emptyLabel, tableView: searchView.tableView)
     }
     
     func getTotalPage() -> Int {
-        if viewModel.totalCount % 30 == 0 {
-            return viewModel.totalCount / 30
+        if viewModel.totalCount % perPage == 0 {
+            return viewModel.totalCount / perPage
         } else {
-            return viewModel.totalCount / 30 + 1
+            return viewModel.totalCount / perPage + 1
         }
     }
     
-    @objc func searchButtonClicked() {
+    func getSearchedDataByNewKeyword() {
         viewModel.imageURL = []
         viewModel.userInform = []
         viewModel.pageNum = 1
         
         fetchData()
+    }
+    
+    // MARK: Actions
+    @objc func searchButtonClicked() {
+        getSearchedDataByNewKeyword()
     }
     
     @objc func cancelButtonClicked() {
@@ -60,6 +71,7 @@ class SearchViewController: UIViewController {
     }
 }
 
+// MARK: UITableViewDataSource & Delegate
 extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.userInform.count
@@ -67,7 +79,6 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath) as? CustomCell else { return UITableViewCell() }
-        
         cell.nameLabel.text = viewModel.userInform[indexPath.row].login
         cell.urlLabel.text = viewModel.userInform[indexPath.row].htmlUrl
         
@@ -80,7 +91,6 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let urls = URL(string: viewModel.userInform[indexPath.row].htmlUrl) else { return }
-        
         UIApplication.shared.open(urls)
     }
     
@@ -89,33 +99,28 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     }
 }
 
+// MARK: UITableViewDataSourcePrefetching
 extension SearchViewController: UITableViewDataSourcePrefetching {
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         if viewModel.pageNum < getTotalPage() {
-            viewModel.pageNum += 1
-            fetchData()
+            if viewModel.userInform.count == viewModel.pageNum * perPage {
+                viewModel.pageNum += 1
+                fetchData()
+            }
         }
     }
 }
 
+// MARK: UISearchbarDelefate
 extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         guard let text = searchBar.text else { return }
-        
-        if text.isEmpty {
-            searchView.cancelButton.isHidden = true
-        } else {
-            searchView.cancelButton.isHidden = false
-        }
+        searchView.cancelButton.isHidden = text.isEmpty ? true : false
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let text = searchView.searchBar.text else { return }
-        
-        viewModel.imageURL = []
-        viewModel.userInform = []
-        viewModel.pageNum = 1
-        viewModel.setProvideData(query: (text, viewModel.pageNum), emptyLabel: searchView.emptyLabel, tableView: searchView.tableView)
+        getSearchedDataByNewKeyword()
         searchBar.resignFirstResponder()
     }
 }
+
